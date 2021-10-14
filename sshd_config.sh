21af7b2253365a314ad393c1ff6c91b0cfc117f3
Peter Korduan <peter.korduan@gdi-service.de>
2021-10-14 17:27:23 +0300

    User privileges root and sudo user

--- sshd_config.sh
+++ sshd_config.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
+
+ROOT_UID=0     # Only users with $UID 0 have root privileges.
+E_NOTROOT=87   # Non-root exit error.
+
+# Run as root, of course.
+if [ "$UID" -ne "$ROOT_UID" ]
+then
+  echo "Must be root to run this script."
+  exit $E_NOTROOT
+fi
+
 ssh_directory="/etc/ssh/sshd_config"
-
-
 AllowTcpForwarding="$(cat $ssh_directory | grep '^#'AllowTcpForwarding[[:space:]]yes)" # #AllowTcpForwarding yes seklinde olanlari gösterir.
+#ClientAliveCountMax=$"(cat $ssh_directory | grep '^#'ClientAliveCountMax)"
 
 if [ "$AllowTcpForwarding" == "#AllowTcpForwarding yes" ];
 then
-echo "$AllowTcpForwarding"
 sed -i s/#AllowTcpForwarding\ yes/AllowTcpForwarding\ no/g $ssh_directory
-echo "has been changed AllowTcpForwarding no"
+echo "$AllowTcpForwarding has been changed AllowTcpForwarding no"
 else
 echo "AllowTcpForwarding no already updated"
 fi
+
+
+#if [ "$ClientAliveCountMax" == "#ClientAliveCountMax" ];
+#then
+#sed -i s/#ClientAliveCountMax\ 3/ClientAliveCountMax\ 2/g $ssh_directory
+#echo "$ClientAliveCountMax has been changed ClientAliveCountMax 2"
+#else
+#echo "ClientAliveCountMax 2 already updated"
+#fi
+
