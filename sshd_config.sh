0d6841bdf28075a5926e37565a6a93f160074cc3
Koray Ak <koray.ak@gdi-service.de>
2021-10-19 16:58:18 +0300

    Change new script

--- sshd_config.sh
+++ sshd_config.sh
@@ -3,54 +3,49 @@
 ROOT_UID=0     # Only users with $UID 0 have root privileges.
 E_NOTROOT=87   # Non-root exit error.
 
-# Run as root, of course.
+# Run as Root or Sudo User, of course.
 if [ "$UID" -ne "$ROOT_UID" ]
 then
-  echo "Must be root to run this script."
+  echo "Must be Root or SUDO User to run this script."
   exit $E_NOTROOT
 fi
 
-
 ssh_directory="/etc/ssh/sshd_config"
-AllowTcpForwarding="$(cat $ssh_directory | grep '^#'AllowTcpForwarding[[:space:]]yes)" # #AllowTcpForwarding yes seklinde olanlari gösterir.
-#ClientAliveCountMax=$"(cat $ssh_directory | grep '^#'ClientAliveCountMax)"
 
- if [ "$AllowTcpForwarding" == "#AllowTcpForwarding yes" ];
-then
-	sed -i s/#AllowTcpForwarding\ yes/AllowTcpForwarding\ no/g $ssh_directory
-	echo "$AllowTcpForwarding has been changed AllowTcpForwarding no"
-else
-	echo "AllowTcpForwarding no already updated"
- fi
+read -p "Change the default SSH port22: " SSH_PORT
+        sed -i \
+        -e "s|#Port 22|Port ${SSH_PORT}|g" \
+        -e "s|#ClientAliveCountMax 3|ClientAliveCountMax 2|g" \
+        -e "s|#PermitRootLogin prohibit-password|PermitRootLogin no|g" \
+	-e "s|#AllowTcpForwarding yes|AllowTcpForwarding no|g" \
+	-e "s|#Compression delayed|Compression no|g" \
+	-e "s|#LogLevel INFO|LogLevel VERBOSE|g" \
+	-e "s|#MaxAuthTries 6|MaxAuthTries 2|g" \
+	-e "s|#MaxSessions 10|MaxSessions 2|g" \
+	-e "s|#TCPKeepAlive yes|TCPKeepAlive no|g"\
+	-e "s|X11Forwarding yes|X11Forwarding no|g" \
+	-e "s|#AllowAgentForwarding yes|AllowAgentForwarding no|g" \
+	-e "s|#LoginGraceTime 2m|LoginGraceTime 1m|g" \
+        $ssh_directory
+        echo "#Port 22 has been changed $SSH_PORT"
+        echo "#ClientAliveCountMax 3 has been changed ClientAliveCountMax 2"
+        echo "#PermitRootLogin prohibit-password has been changed PermitRootLogin no"
+	echo "#AllowTcpForwarding yes has been changed AllowTcpForwarding no"
+	echo "#Compression delayed has been changed Compression no"
+	echo "#LogLevel INFO has been changed LogLevel VERBOSE"
+	echo "#MaxAuthTries 6 has been changed MaxAuthTries 2"
+	echo "#MaxSessions 10 has been changed MaxSessions 2"
+	echo "#TCPKeepAlive yes has been changed TCPKeepAlive no"
+	echo "X11Forwarding yes has been changed X11Forwarding no"
+	echo "#AllowAgentForwarding yes has been changed AllowAgentForwarding no"
+	echo "#LoginGraceTime 2m has been changed LoginGraceTime 1m"
+        systemctl reload ssh
 
-Compression="$(cat $ssh_directory | grep '^#'Compression[[:space:]]delayed)" #
-
-
-if [ "$Compression" == "#Compression delayed" ];
-then
-sed -i s/#Compression\ delayed/Compression\ no/g $ssh_directory
-echo "$Compression has been changed Compression no"
-else
-echo "Compression no already updated"
-fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-#if [ "$ClientAliveCountMax" == "#ClientAliveCountMax" ];
-#then
-#sed -i s/#ClientAliveCountMax\ 3/ClientAliveCountMax\ 2/g $ssh_directory
-#echo "$ClientAliveCountMax has been changed ClientAliveCountMax 2"
+#This option is not mandatory,if you want to prevent from restrict number of sessions via SSH connections, you can use.
+#etc_security_path="/etc/security/limits.conf"
+#maxlogins="gisadmin - maxlogins  2"
+#if grep -q "$maxlogins" "$etc_security_path" ; then
+#	exit
 #else
-#echo "ClientAliveCountMax 2 already updated"
+#	echo "$maxlogins" >> $etc_security_path
 #fi
-
